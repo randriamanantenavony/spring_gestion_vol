9b23f77b7aabdca2d0ede74c99ecd870966e0aa5
randriamanantenavony <152492886+randriamanantenavony@users.noreply.github.com>
2025-08-12 23:17:24 +0300

    modif

--- vol/vol/src/main/java/com/ticketing/vol/controller/client/ReservationController.java
+++ vol/vol/src/main/java/com/ticketing/vol/controller/client/ReservationController.java
@@ -1,12 +1,10 @@
 package com.ticketing.vol.controller.client;
 
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import com.ticketing.vol.model.client.DetailReservation;
 import com.ticketing.vol.services.client.ReservationService;
 
 import jakarta.servlet.http.HttpSession;
@@ -22,8 +20,8 @@ public class ReservationController {
     }
 
     @PostMapping("/annuler")
-    public String annulerReservation(@RequestParam("idReservationDetail") Long idReservationDetail, HttpSession session, Model model) {
-        reservationService.annulerReservation(idReservationDetail);
+    public String annulerReservation(@RequestParam("reservationId") Long idReservation, HttpSession session, Model model) {
+        reservationService.annulerReservation(idReservation);
         return "redirect:/client/success";
     }
 }
